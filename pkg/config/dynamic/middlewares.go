c17fabcfa14355a34f496c1a258fa05ece258953
Giedrius M <giedrius.masalskis@softra.lt>
2023-01-09 11:59:37 +0200

    ForwardAuth middleware: add parameter forwardBody

--- pkg/config/dynamic/middlewares.go
+++ pkg/config/dynamic/middlewares.go
@@ -216,6 +216,8 @@ type ForwardAuth struct {
 	// AuthRequestHeaders defines the list of the headers to copy from the request to the authentication server.
 	// If not set or empty then all request headers are passed.
 	AuthRequestHeaders []string `json:"authRequestHeaders,omitempty" toml:"authRequestHeaders,omitempty" yaml:"authRequestHeaders,omitempty" export:"true"`
+	// Request body forwarding configuration
+	ForwardBody bool `json:"forwardBody,omitempty" toml:"forwardBody,omitempty" yaml:"forwardBody,omitempty" export:"true"`
 }
 
 // +k8s:deepcopy-gen=true
